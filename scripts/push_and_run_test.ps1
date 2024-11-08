86f3e234fb9327a822a2ac21b80e53079ae5cf40
hongruichen <chraac@gmail.com>
2024-11-08 14:16:10 +0800

    feat: update push script, add llama-bench

--- scripts/push_and_run_test.ps1
+++ scripts/push_and_run_test.ps1
@@ -1,6 +1,6 @@
 
 $_is_push_only = $false
-$_device_path = '/data/local/tmp/'
+$_device_path = '/data/local/tmp'
 $_script_path = Split-Path -Parent $MyInvocation.MyCommand.Path
 $_build_path = "$_script_path/../build_qnn"
 
@@ -10,31 +10,38 @@ if ($args.Length -eq 1) {
     }
 }
 
-Get-ChildItem -Path "$_build_path/*.so" | ForEach-Object { adb push $_.FullName $_device_path }
+Get-ChildItem -Path "$_build_path/*.so" | ForEach-Object { adb push $_.FullName "$_device_path/" }
 
-$test_executable_name = 'test-backend-ops'
-$test_executable_full_path = "$_device_path$test_executable_name"
-adb push "$_build_path/$test_executable_name" $_device_path
-adb shell "chmod +x $test_executable_full_path"
-
-$executable_name = 'llama-cli'
-$executable_full_path = "$_device_path$executable_name"
-adb push "$_build_path/llama-cli" $_device_path
-adb shell "chmod +x $executable_full_path"
-
-if (Test-Path "$_build_path/lldb-server") {
-    adb push "$_build_path/lldb-server" $_device_path
-    adb shell "chmod +x ${_device_path}lldb-server"
-} else {
-    Write-Host -ForegroundColor Yellow "lldb-server not found in $_build_path"
+function Push-Executable {
+    param (
+        $exec_name
+    )
+    
+    $exec_full_path = "$_device_path/$exec_name"
+    adb push "$_build_path/$exec_name" "$_device_path/"
+    adb shell "chmod +x $exec_full_path"
 }
 
-if (Test-Path "$_build_path/gdbserver") {
-    adb push "$_build_path/gdbserver" $_device_path
-    adb shell "chmod +x ${_device_path}gdbserver"
-} else {
-    Write-Host -ForegroundColor Yellow "gdbserver not found in $_build_path"
+Push-Executable 'test-backend-ops'
+Push-Executable 'llama-cli'
+Push-Executable 'llama-bench'
+
+function Push-Debug-Server {
+    param (
+        $server_name
+    )
+    
+    if (Test-Path "$_build_path/$server_name") {
+        adb push "$_build_path/$server_name" "$_device_path/"
+        adb shell "chmod +x $_device_path/$server_name"
+    }
+    else {
+        Write-Host -ForegroundColor Red "$server_name not found in $_build_path"
+    }
 }
+
+Push-Debug-Server 'lldb-server'
+Push-Debug-Server 'gdbserver'
 
 if (!$_is_push_only) {
     Write-Host -ForegroundColor Green "Running tests GGML_OP_ADD ..."
