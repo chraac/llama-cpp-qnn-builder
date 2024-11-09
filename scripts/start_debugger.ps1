17fa589c6f040fae06d4f0affac6436822de547d
hongruichen <chraac@gmail.com>
2024-11-09 13:44:19 +0800

    feat: [scripts] add parameters to config the executable

--- scripts/start_debugger.ps1
+++ scripts/start_debugger.ps1
@@ -1,16 +1,26 @@
 
-$_device_path = '/data/local/tmp/'
-$_port = 23456
-$_parameters = 'test -o MUL_MAT'
-$_should_forward_port = $false
+param (
+    [string]$_device_path = '/data/local/tmp/',
+    [int]$_port = 23456,
+    [string]$_parameters = 'test -o MUL_MAT',
+    [switch]$_should_forward_port = $false,
+    [string]$_executable_name = 'test-backend-ops'
+)
 
-if ($args.Length -eq 1) {
-    if ($args[0] -eq '-p') {
+# loop through the arguments
+for ($i = 0; $i -lt $args.Length; $i++) {
+    if ($args[$i] -eq '-p') {
         $_should_forward_port = $true
+    }
+    elseif ($args[$i] -eq '-exec') {
+        $_executable_name = $args[$i + 1]
+    }
+    elseif ($args[$i] -eq '-params') {
+        $_parameters = $args[$i + 1]
     }
 }
 
-$job = Start-job { adb shell $args[0] } -ArgumentList "cd $_device_path && ./gdbserver :$_port ./test-backend-ops $_parameters"
+$job = Start-job { adb shell $args[0] } -ArgumentList "cd $_device_path && ./gdbserver :$_port ./$_executable_name $_parameters"
 
 if ($_should_forward_port) {
     adb forward tcp:$_port tcp:$_port
