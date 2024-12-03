4e7597563e77fa75b81e24467bf5976dfa603c03
hongruichen <chraac@gmail.com>
2024-12-03 23:41:36 +0800

    wip

--- scripts/start_debugger.sh
+++ scripts/start_debugger.sh
@@ -2,16 +2,22 @@
 
 DEVICE_PATH='/data/local/tmp/'
 PORT=23456
-PARAMETERS='test -o MUL_MAT'
+PARAMETERS='test'
 SHOULD_FORWARD_PORT=false
 EXECUTABLE_NAME='test-backend-ops'
 
 # Parse arguments
 while [[ "$#" -gt 0 ]]; do
     case $1 in
-        -p) SHOULD_FORWARD_PORT=true ;;
-        -exec) EXECUTABLE_NAME="$2"; shift ;;
-        -params) PARAMETERS="$2"; shift ;;
+    -p) SHOULD_FORWARD_PORT=true ;;
+    -exec)
+        EXECUTABLE_NAME="$2"
+        shift
+        ;;
+    -params)
+        PARAMETERS="$2"
+        shift
+        ;;
     esac
     shift
 done
@@ -21,7 +27,7 @@ adb shell "cd $DEVICE_PATH && ./gdbserver :$PORT ./$EXECUTABLE_NAME $PARAMETERS"
 
 # Forward the port if needed
 if [ "$SHOULD_FORWARD_PORT" = true ]; then
-    adb forward -a tcp:$PORT tcp:$PORT
+    adb forward tcp:$PORT tcp:$PORT
     adb forward --list
 fi
 
