3b2b5af8152f65badd5afeb152877ae8fc18da21
hongruichen <chraac@gmail.com>
2025-01-06 23:31:41 +0800

    fix script error

--- scripts/run_benchmark_and_save_log.sh
+++ scripts/run_benchmark_and_save_log.sh
@@ -22,7 +22,7 @@ done
 
 LOG_FILE_PATH="$SCRIPT_PATH/../$LOG_FILE_NAME"
 # adb shell 'cd /data/local/tmp/ && LLAMA_CACHE=/data/local/tmp/cache ./llama-bench --progress -v -mmp 0 -m meta-llama_Meta-Llama-3.2-1B-Instruct-f32.gguf' > llama-bench-f32-qnn-gpu-debug.log 2>&1
-COMMAND_STRING='cd $DEVICE_PATH && '
+COMMAND_STRING="cd $DEVICE_PATH && "
 COMMAND_STRING+="LLAMA_CACHE=$DEVICE_PATH/cache "
 COMMAND_STRING+='./llama-bench --progress -v -mmp 0 -m meta-llama_Meta-Llama-3.2-1B-Instruct-f32.gguf'
 adb shell $COMMAND_STRING >$LOG_FILE_PATH 2>&1
