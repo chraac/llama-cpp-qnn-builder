c39d1ae3bc1964f508dac0a3655ff50110900901
hongruichen <chraac@gmail.com>
2025-02-15 13:55:13 +0800

    wip

--- docker/build_in_container.sh
+++ docker/build_in_container.sh
@@ -17,16 +17,17 @@ set -e
 _android_ndk_options="-DANDROID_ABI=$TARGET_ARCH -DANDROID_PLATFORM=$ANDROID_PLATFORM -DANDROID_NDK=$ANDROID_NDK_HOME -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake"
 cmake -H.. -B. -DGGML_QNN=on $CMAKE_EXTRA_OPTIONS $_android_ndk_options -DGGML_QNN_SDK_PATH="$QNN_SDK_PATH" -DCMAKE_BUILD_TYPE="$BUILD_TYPE"
 make -j $(nproc)
+_qnn_libs_path="$QNN_SDK_PATH/lib/aarch64-android"
 chmod -R u+rw $OUTPUT_DIR
 rsync -av ./bin/llama-* $OUTPUT_DIR
 rsync -av ./bin/test-backend-ops $OUTPUT_DIR
-rsync -av $QNN_SDK_PATH/lib/aarch64-android/libQnnSystem.so $OUTPUT_DIR
-rsync -av $QNN_SDK_PATH/lib/aarch64-android/libQnnCpu.so $OUTPUT_DIR
-rsync -av $QNN_SDK_PATH/lib/aarch64-android/libQnnGpu.so $OUTPUT_DIR
-rsync -av $QNN_SDK_PATH/lib/aarch64-android/libQnnHtp.so $OUTPUT_DIR
-rsync -av $QNN_SDK_PATH/lib/aarch64-android/libQnnHtpNetRunExtensions.so $OUTPUT_DIR
-rsync -av $QNN_SDK_PATH/lib/aarch64-android/libQnnHtpPrepare.so $OUTPUT_DIR
-rsync -av $QNN_SDK_PATH/lib/aarch64-android/libQnnHtp*Stub.so $OUTPUT_DIR
+rsync -av $_qnn_libs_path/libQnnSystem.so $OUTPUT_DIR
+rsync -av $_qnn_libs_path/libQnnCpu.so $OUTPUT_DIR
+rsync -av $_qnn_libs_path/libQnnGpu.so $OUTPUT_DIR
+rsync -av $_qnn_libs_path/libQnnHtp.so $OUTPUT_DIR
+rsync -av $_qnn_libs_path/libQnnHtpNetRunExtensions.so $OUTPUT_DIR
+rsync -av $_qnn_libs_path/libQnnHtpPrepare.so $OUTPUT_DIR
+rsync -av $_qnn_libs_path/libQnnHtp*Stub.so $OUTPUT_DIR
 rsync -av $QNN_SDK_PATH/lib/hexagon-*/unsigned/libQnnHtp*Skel.so $OUTPUT_DIR
 rsync -av $ANDROID_NDK_HOME/prebuilt/android-arm64/gdbserver/gdbserver $OUTPUT_DIR
 chown -R "$HOST_USER_ID" "$OUTPUT_DIR"
