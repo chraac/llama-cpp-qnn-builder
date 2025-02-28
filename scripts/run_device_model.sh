1048761bcd17985ce391fa482c27d441ee981a81
hongruichen <chraac@gmail.com>
2025-02-28 11:23:28 +0800

    fix default name

--- scripts/run_device_model.sh
+++ scripts/run_device_model.sh
@@ -3,7 +3,7 @@
 _script_path=$(dirname "$(realpath "$0")")
 _device_exec_path='/data/local/tmp'
 _device_model_path='/data/local/tmp'
-_model_name='meta-llama_Meta-Llama-3.2-1B-f32.gguf'
+_model_name='meta-llama_Meta-Llama-3.2-1B-Instruct-f32.gguf'
 
 # parse arguments to get the log file name
 while [[ $# -gt 0 ]]; do
