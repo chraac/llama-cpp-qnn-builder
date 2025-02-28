2af083d784172139c1ef3ed536b4efbd7276cd2a
hongruichen <chraac@gmail.com>
2025-02-28 12:37:45 +0800

    add option to enable verbose log

--- scripts/run_device_model.sh
+++ scripts/run_device_model.sh
@@ -4,6 +4,7 @@ _script_path=$(dirname "$(realpath "$0")")
 _device_exec_path='/data/local/tmp'
 _device_model_path='/data/local/tmp'
 _model_name='meta-llama_Meta-Llama-3.2-1B-Instruct-f32.gguf'
+_extra_args=''
 
 # parse arguments to get the log file name
 while [[ $# -gt 0 ]]; do
@@ -12,6 +13,10 @@ while [[ $# -gt 0 ]]; do
     --model-name)
         _model_name="$2"
         shift
+        shift
+        ;;
+    --verbose)
+        _extra_args="-v"
         shift
         ;;
     *)
@@ -23,6 +28,6 @@ done
 
 device_command_string="cd $_device_exec_path && "
 device_command_string+="LLAMA_CACHE=$_device_exec_path/cache "
-device_command_string+="./llama-cli -m $_device_model_path/${_model_name} --color -i -r \"User:\""
+device_command_string+="./llama-cli $_extra_args -m \"$_device_model_path/${_model_name}\" --color -i -r \"User:\""
 
 adb shell $device_command_string
