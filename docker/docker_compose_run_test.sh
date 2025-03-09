2ee97e9bb1abcf16810299d17e30e8b902bf8674
Hongrui Chen <chraac@gmail.com>
2025-03-09 22:44:22 +0800

    wip

--- docker/docker_compose_run_test.sh
+++ docker/docker_compose_run_test.sh
@@ -4,7 +4,6 @@ _run_logs_dir="$_script_dir/../run_logs"
 set -e
 
 mkdir -p "$_run_logs_dir"
-rm -rf "$_run_logs_dir/test-backend-ops-all-npu.log"
 pushd "$_script_dir"
 _extra_args='--exit-code-from llama-qnn-run'
 _compose_command='docker compose -f docker-compose-run.yml'
