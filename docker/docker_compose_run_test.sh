9a3df29b4bc16db920f95a91c9ba9076c8dfb63c
Hongrui Chen <chraac@gmail.com>
2025-03-09 22:21:52 +0800

    output test log to local file

--- docker/docker_compose_run_test.sh
+++ docker/docker_compose_run_test.sh
@@ -1,7 +1,9 @@
 _script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+_run_logs_dir="$_script_dir/../run_logs"
 
 set -e
 
+mkdir -p "$_run_logs_dir"
 pushd "$_script_dir"
 _extra_args='--exit-code-from llama-qnn-run'
 _compose_command='docker compose -f docker-compose-run.yml'
