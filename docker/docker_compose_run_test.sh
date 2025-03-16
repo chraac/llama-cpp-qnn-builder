2591db357779e065f99373b38a8dd20920eb3417
Hongrui Chen <chraac@gmail.com>
2025-03-16 21:40:53 +0800

    add param to specify backend

--- docker/docker_compose_run_test.sh
+++ docker/docker_compose_run_test.sh
@@ -1,14 +1,32 @@
 _script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 _run_logs_dir="$_script_dir/../run_logs"
+_test_backend='all'
+
+# Parse command-line arguments
+while (("$#")); do
+    case "$1" in
+    -b | --backend)
+        _test_backend="$2"
+        shift 2
+        ;;
+    *) # preserve positional arguments
+        echo "Invalid option $1"
+        exit 1
+        ;;
+    esac
+done
 
 set -e
 
 mkdir -p "$_run_logs_dir"
 pushd "$_script_dir"
+
+export TEST_BACKEND="$_test_backend"
 _extra_args='--exit-code-from llama-qnn-run'
 _compose_command='docker compose -f docker-compose-run.yml'
 $_compose_command build --pull
 $_compose_command up --build $_extra_args
+
 popd
 
 set +e
--- docker/run_in_container.sh
+++ docker/run_in_container.sh
@@ -2,11 +2,17 @@
 
 echo "EXEC_MOUNT_POINT: $EXEC_MOUNT_POINT"
 echo "RUN_LOG_PATH: $RUN_LOG_PATH"
+echo "TEST_BACKEND: $TEST_BACKEND"
 
 cd $EXEC_MOUNT_POINT
 set -e
 
-./test-backend-ops test -b qnn-cpu >$RUN_LOG_PATH/test-backend-ops-all-cpu.log 2>&1
-./test-backend-ops test -b qnn-npu >$RUN_LOG_PATH/test-backend-ops-all-npu.log 2>&1
+if [ "$TEST_BACKEND" = "cpu" ] || [ "$TEST_BACKEND" = "all" ]; then
+    ./test-backend-ops test -b qnn-cpu >$RUN_LOG_PATH/test-backend-ops-all-cpu.log 2>&1
+fi
+
+if [ "$TEST_BACKEND" = "npu" ] || [ "$TEST_BACKEND" = "all" ]; then
+    ./test-backend-ops test -b qnn-npu >$RUN_LOG_PATH/test-backend-ops-all-npu.log 2>&1
+fi
 
 set +e
