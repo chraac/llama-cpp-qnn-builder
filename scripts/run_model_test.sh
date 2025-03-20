77cdf19ad0c8021ed60db313647de53cf0537092
hongruichen <chraac@gmail.com>
2025-03-21 00:09:40 +0800

    add param to set model name

--- scripts/run_model_test.sh
+++ scripts/run_model_test.sh
@@ -3,7 +3,8 @@
 _script_path=$(dirname "$(realpath "$0")")
 _ld_library_path="${_script_path}/../build_qnn_x86_64:$LD_LIBRARY_PATH"
 _llama_cli="${_script_path}/../build_qnn_x86_64/llama-cli"
-_model_full_path="${_script_path}/../run_logs/meta-llama_Meta-Llama-3.2-1B-Instruct-Q4_K_M.gguf"
+_model_name='meta-llama_Meta-Llama-3.2-1B-Instruct-Q4_K_M.gguf'
+_model_path="${_script_path}/../run_logs"
 _prompt="I believe the meaning of life is"
 _log_file_path="${_script_path}/../run_logs/emulator/model_test.log"
 _extra_args='--device none -n 64 --ignore-eos'
@@ -15,6 +16,10 @@ while (("$#")); do
         _extra_args="${_extra_args} -v"
         shift
         ;;
+    -m | --model-name)
+        _model_name="$2"
+        shift 2
+        ;;
     *) # preserve positional arguments
         echo "Invalid option $1"
         exit 1
@@ -22,4 +27,4 @@ while (("$#")); do
     esac
 done
 
-LD_LIBRARY_PATH=${_ld_library_path} $_llama_cli -no-cnv --model $_model_full_path -s 1234 $_extra_args -p "$_prompt" >$_log_file_path 2>&1
+LD_LIBRARY_PATH=${_ld_library_path} $_llama_cli -no-cnv --model "${_model_path}/${_model_name}" -s 1234 $_extra_args -p "$_prompt" >$_log_file_path 2>&1
