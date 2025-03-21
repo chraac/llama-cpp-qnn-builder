c2554e0890170a7c8759af58f7575826e17346ee
hongruichen <chraac@gmail.com>
2025-03-21 22:53:13 +0800

    set log verbosity level

--- scripts/run_local_model.sh
+++ scripts/run_local_model.sh
@@ -5,6 +5,7 @@ _ld_library_path="${_script_path}/../build_qnn_x86_64:$LD_LIBRARY_PATH"
 _llama_cli="${_script_path}/../build_qnn_x86_64/llama-cli"
 _model_name='meta-llama_Meta-Llama-3.2-1B-Instruct-Q4_K_M.gguf'
 _model_path="${_script_path}/../models"
+_verbose_log=0
 _extra_args=''
 
 # parse arguments to get the log file name
@@ -17,7 +18,7 @@ while [[ $# -gt 0 ]]; do
         shift
         ;;
     -v | --verbose)
-        _extra_args="-v"
+        _verbose_log=1
         shift
         ;;
     *)
@@ -26,5 +27,11 @@ while [[ $# -gt 0 ]]; do
         ;;
     esac
 done
+
+if [ $_verbose_log -eq 1 ]; then
+    _extra_args="${_extra_args} -v"
+else
+    _extra_args="${_extra_args} --verbosity 0"
+fi
 
 LD_LIBRARY_PATH=${_ld_library_path} $_llama_cli $_extra_args -m "$_model_path/$_model_name" --no-mmap --color -i -r "User:"
