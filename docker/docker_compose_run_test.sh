f14115a0cdb3602749cef5570cb64c1c535726f0
hongruichen <chraac@gmail.com>
2025-03-22 12:11:02 +0800

    wip

--- docker/docker_compose_run_test.sh
+++ docker/docker_compose_run_test.sh
@@ -16,17 +16,26 @@ while (("$#")); do
     esac
 done
 
-set -e
-
 mkdir -p "$_run_logs_dir"
 pushd "$_script_dir"
+
+set -e
 
 export TEST_BACKEND="$_test_backend"
 _extra_args='--exit-code-from llama-qnn-run'
 _compose_command='docker compose -f docker-compose-run.yml'
 $_compose_command build --pull
-$_compose_command up --build $_extra_args
-
-popd
 
 set +e
+
+$_compose_command up --build $_extra_args
+
+# check return code
+if [ $? -ne 0 ]; then
+    echo -e "\e[31mTest failed!\e[0m"
+    exit 1
+fi
+
+echo -e "\e[32mTest passed!\e[0m"
+
+popd
