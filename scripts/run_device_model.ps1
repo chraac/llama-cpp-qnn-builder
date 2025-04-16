05b24a3f30bfe53e7793f1bcfb1ff48cd75cd55f
hongruichen <chraac@gmail.com>
2025-04-16 18:20:09 +0800

    wip

--- scripts/run_device_model.ps1
+++ scripts/run_device_model.ps1
@@ -1,30 +1,45 @@
-
-# Parse command-line arguments
 param (
-    [string]$_modelName = 'meta-llama_Meta-Llama-3.2-1B-Instruct-f32.gguf',    
-    [string]$_extraArgs = ''
+    [string]$ModelName = 'meta-llama_Meta-Llama-3.2-1B-Instruct-f32.gguf',
+    [switch]$Verbose,
+    [switch]$PushToDevice
 )
 
-$_scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Path
-$_deviceExecPath = '/data/local/tmp'
-$_deviceModelPath = '/data/local/tmp'
+$scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Path
+$deviceExecPath = '/data/local/tmp'
+$deviceModelPath = '/data/local/tmp'
+$extraArgs = ''
 
-# loop through the arguments
-for ($i = 0; $i -lt $args.Length; $i++) {
-    if ($args[$i] -eq '--model-name') {
-        $_modelName = $args[$i + 1]
-    } elseif ($args[$i] -eq '--verbose') {
-        $_extraArgs = '-v '
+# Process non-parameter arguments for backward compatibility
+foreach ($arg in $args) {
+    switch ($arg) {
+        '-m' { $ModelName = $args[$args.IndexOf($arg) + 1] }
+        '--model-name' { $ModelName = $args[$args.IndexOf($arg) + 1] }
+        '-v' { $Verbose = $true }
+        '--verbose' { $Verbose = $true }
+        '-p' { $PushToDevice = $true }
+        '--push-to-device' { $PushToDevice = $true }
+        default {
+            # Skip argument values that follow parameter names
+            if ($args[$args.IndexOf($arg) - 1] -notin @('-m', '--model-name')) {
+                Write-Host "Invalid option $arg"
+                exit 1
+            }
+        }
     }
 }
 
-Write-Host "Model name: $_modelName"
-Write-Host "Extra Arguments: $_extraArgs"
+if ($Verbose) {
+    $extraArgs = "-v"
+}
+
+if ($PushToDevice) {
+    & "$scriptPath/push_and_run_test.ps1" -p
+}
 
 # Build the device command string
-$deviceCommandString = "cd $_deviceExecPath && "
-$deviceCommandString += "LLAMA_CACHE=$_deviceExecPath/cache "
-$deviceCommandString += "./llama-cli $_extraArgs -m `"$_deviceModelPath/$_modelName`" --color -i -r `"User:`""
+$deviceCommandString = "cd $deviceExecPath && "
+$deviceCommandString += "LLAMA_CACHE=$deviceExecPath/cache "
+$deviceCommandString += "./llama-cli $extraArgs -m `"$deviceModelPath/$ModelName`" --no-mmap --color -i -r `"User:`""
 
 # Execute the ADB command
 adb shell $deviceCommandString
