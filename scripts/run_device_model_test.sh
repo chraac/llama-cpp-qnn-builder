7eef0d128fcb682a59a287703309b17a356c33d6
chraac <chraac@gmail.com>
2026-05-02 12:29:59 +0800

    wip

--- scripts/run_device_model_test.sh
+++ scripts/run_device_model_test.sh
@@ -83,7 +83,7 @@ fi
 
 device_command_string="cd $_device_exec_path && "
 device_command_string+="GGML_HEXAGON_EXPERIMENTAL=1 LLAMA_CACHE=$_device_exec_path/.cache LD_LIBRARY_PATH=./ ADSP_LIBRARY_PATH=./ "
-device_command_string+="./llama-completion $extra_args -m '$_device_model_path/${_model_name}' --fit on -no-cnv -s 1234 -c 4096 -p '$_prompt_string'"
+device_command_string+="./llama-completion $_extra_args -m '$_device_model_path/${_model_name}' --fit on -no-cnv -s 1234 -c 4096 -p '$_prompt_string'"
 
 adb logcat -c
 adb logcat -s 'adsprpc' 'llama-completion' 'pid-' >$_logcat_output_path 2>&1 &
