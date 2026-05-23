7b881881ba2888814a4e7ee90f7c3c7d983d315a
chraac <chraac@gmail.com>
2026-05-23 12:19:12 +0800

    fix: add LLAMA_BUILD_WEBUI option to build options in docker script

--- docker/docker_compose_compile.sh
+++ docker/docker_compose_compile.sh
@@ -10,7 +10,7 @@ _in_ci=0
 _pull_latest=0
 _build_platform='android' # default build platform, could be 'android' or 'linux'
 _build_arch='arm64-v8a'   # default build arch, could be 'arm64-v8a' or 'x86_64'
-_build_options='-DBUILD_SHARED_LIBS=off -DLLAMA_CURL=off'
+_build_options='-DBUILD_SHARED_LIBS=off -DLLAMA_CURL=off -DLLAMA_BUILD_WEBUI=off'
 _extra_build_options=''
 _run_backend_tests=0
 _enable_hexagon_backend=1
